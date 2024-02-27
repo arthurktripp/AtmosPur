3d82cc8eee0edb8f9f00e9807a1489f43ef3fde2
Arthur Tripp <arthur.k.tripp@gmail.com>
2024-02-26 22:42:00 -0800

    Hide and warn on location change

--- static/js/interactive.js
+++ static/js/interactive.js
@@ -1,6 +1,24 @@
 function locationWarn() {
-  if (confirm("Changing this setting will affect long-term tracking of outside AQI. Continue?"))
+  if (confirm("Changing this setting will affect long-term tracking of outside AQI. Continue?")) {
     return true;
-  else
+  } else {
     return false;
+  }
 }
+
+
+
+function showLocUpdate() {
+  locForm = document.getElementById("location-update");
+  locForm.style.visibility = "visible";
+}
+
+function hideLocUpdate() {
+  locForm = document.getElementById("location-update");
+  locForm.style.visibility = "hidden";
+}
+
+cancelButton = document.getElementById("cancel-location-update")
+cancelButton.addEventListener('click', function(event) {
+  event.preventDefault()
+})
