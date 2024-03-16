e82bed4defa9587a65e207006b0815e37370a0d1
Arthur Tripp <arthur.k.tripp@gmail.com>
2024-03-16 14:26:58 -0700

    removed print statements used in testing

--- static/microservices/sensorService.py
+++ static/microservices/sensorService.py
@@ -2,33 +2,30 @@ import serial, time, requests, math
 
 
 # check USB port with "python3 -m serial.tools.list_ports"
+sensor = serial.Serial('/dev/cu.usbmodem143401', 9600, timeout=5)
 
-ser = serial.Serial('/dev/cu.usbmodem143401', 9600, timeout=5)
-url = "http://127.0.0.1:5001/api/inside"
-
-
-def push_to_app(json_obj):
-  response = requests.post(url, json=json_obj)
-  if response.status_code == 201:
-    print('Message received by server')
-  else:
-    print('There was a problem: ', response.status_code)
-  return
-  
 
 def get_current_aqi():
-  data_stream = str(ser.readline())
+  data_stream = str(sensor.readline())
   data_stream = data_stream[2:-3]
   signal = int(data_stream[0:-2])
   aqi = math.floor((signal / 10) ** 1.5)
   
   time_raw = time.localtime()
   time_log = time.asctime(time_raw)
-  print(aqi)
-  aqi_json = {"inside-aqi": aqi,
+
+  aqi_info = {"inside-aqi": aqi,
               "time": time_log}
-  
-  push_to_app(aqi_json)
+  push_to_app(aqi_info)
+  return
+
+
+def push_to_app(aqi_data):
+  response = requests.post("http://127.0.0.1:5001/api/inside", json=aqi_data)
+  if response.status_code == 201:
+    print('Message received by server')
+  else:
+    print('There was a problem: ', response.status_code)
   return
   
 
