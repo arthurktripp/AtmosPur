adbaf1485bfd8ee0ec81f754881dd1345738341a
Arthur Tripp <arthur.k.tripp@gmail.com>
2024-03-16 14:51:35 -0700

    rmv unused imports and if statements; add comments

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from flask import Flask, jsonify, request, render_template, redirect, url_for
-from flask_socketio import SocketIO
+from flask import Flask, request, render_template, redirect
 import json, time
 import static.microservices.healthinfo as healthinfo
 import static.microservices.makegraph as makegraph
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "password!"
-socketio = SocketIO(app)
-
-# location_service = subprocess.run(['python3', './static/microservices/locationService.py'], shell=True, capture_output=True, text=True)
 
 inside_aqi = 0
 outside_aqi = 0
@@ -20,81 +15,73 @@ def check_settings(key):
   return userSettingsData[key]
  
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-  if request.method == "GET":
-    insideData = healthinfo.getHealthData(inside_aqi)
-    outsideData = healthinfo.getHealthData(outside_aqi)
-    location_search = check_settings("home-location")
-    location_name = check_settings("location-name")
-    title = "AtmosPür"
-        
-    return render_template('index.html',
-                          current_active = True,
-                          title = title,
-                          page_title = "Current Stats",
-                          inside_aqi = inside_aqi,
-                          outside_aqi = outside_aqi,
-                          location_name = location_name,
-                          location_search = location_search,
-                          insideTitle = insideData["title"],
-                          insideRange = insideData["range string"],
-                          insideTagline = insideData["tagline"],
-                          insideBody = insideData["body"],
-                          outsideTitle = outsideData["title"],
-                          outsideRange = outsideData["range string"],
-                          outsideTagline = outsideData["tagline"],
-                          outsideBody = outsideData["body"]                          
-                          )
+  insideData = healthinfo.getHealthData(inside_aqi)
+  outsideData = healthinfo.getHealthData(outside_aqi)
+  location_search = check_settings("home-location")
+  location_name = check_settings("location-name")
+  title = "AtmosPür"
+      
+  return render_template('index.html',
+                        current_active = True,
+                        title = title,
+                        page_title = "Current Stats",
+                        inside_aqi = inside_aqi,
+                        outside_aqi = outside_aqi,
+                        location_name = location_name,
+                        location_search = location_search,
+                        insideTitle = insideData["title"],
+                        insideRange = insideData["range string"],
+                        insideTagline = insideData["tagline"],
+                        insideBody = insideData["body"],
+                        outsideTitle = outsideData["title"],
+                        outsideRange = outsideData["range string"],
+                        outsideTagline = outsideData["tagline"],
+                        outsideBody = outsideData["body"]                          
+                        )
 
 
 @app.route("/PurProgress", methods=["GET"])
 def purprogress():
-  if request.method == "GET":
-    title = "PürProgress"
-    location_search = check_settings("home-location")
-    location_name = check_settings("location-name")
-    graph_type = request.args.get('graph-type')
-    graph = makegraph.graph_aqi(graph_type)
-    
-    return render_template('pur-progress.html',
-                           purprogress_active = True,
-                           title = title,
-                           page_title = "PürProgress",
-                           location_name = location_name,
-                           location_search = location_search,
-                           graph = graph)
-
-
-
+  title = "PürProgress"
+  location_search = check_settings("home-location")
+  location_name = check_settings("location-name")
+  graph_type = request.args.get('graph-type')
+  graph = makegraph.graph_aqi(graph_type)
+  
+  return render_template('pur-progress.html',
+                          purprogress_active = True,
+                          title = title,
+                          page_title = "PürProgress",
+                          location_name = location_name,
+                          location_search = location_search,
+                          graph = graph)
 
 
 @app.route("/location_search", methods=["POST"])
+# to set the home location
 def location_search():
-  if request.method == "POST":
-    location_search = request.form['locationInput']
-    with open('./static/microservices/user-settings.json', "r") as userSettings:
-      userSettingsData = json.load(userSettings)
+  location_search = request.form['locationInput']
+  with open('./static/microservices/user-settings.json', "r") as userSettings:
+    userSettingsData = json.load(userSettings)
 
-    if location_search != "":
-      location_search += " USA"
-      userSettingsData['location-search'] = location_search
+  if location_search != "":
+    location_search += " USA"
+    userSettingsData['location-search'] = location_search
 
-    with open('./static/microservices/user-settings.json', "w") as userSettings:
-      json.dump(userSettingsData, userSettings)
+  with open('./static/microservices/user-settings.json', "w") as userSettings:
+    json.dump(userSettingsData, userSettings)
 
-    print('New Location is: ', location_search)
-    time.sleep(1)
-    return redirect('/')
-
+  print('New Location is: ', location_search)
+  time.sleep(1)
+  return redirect('/')
 
 
 
+# ---- API ENDPOINTS ---- #
 
-
-
-
-# API ENDPOINTS:
+# Receive current inside AQI
 @app.route("/api/inside", methods=["POST"])
 def post_listener_inside():
   data = request.get_json()
@@ -103,6 +90,7 @@ def post_listener_inside():
   return "AQI Updated", 201
 
 
+# Receive current outside AQI
 @app.route("/api/outside", methods=["POST"])
 def post_listener_outside():
   data = request.get_json()
@@ -111,6 +99,8 @@ def post_listener_outside():
   return "AQI Updated", 201
 
 
+# reports the current AQIs and time
+# used by the data logging service
 @app.route("/api/data_tracking", methods=["GET"])
 def send_data():
   time_raw = time.localtime()
@@ -120,11 +110,5 @@ def send_data():
   return json.dumps(data)
 
 
-
-
-
 if __name__ == "__main__":
   app.run(port=5001, debug = True)
-  # socketio.run(app)
-
-
